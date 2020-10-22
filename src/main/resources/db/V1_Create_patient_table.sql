86782f1488f5e3032eb5739e32d0ac318d439211
dimanoviy <54929272+dimanoviy@users.noreply.github.com>
2020-10-22 11:29:56 +0700

    Delete V1_Create_patient_table.sql

--- src/main/resources/db/V1_Create_patient_table.sql
+++
@@ -1,7 +0,0 @@
-create table PATIENT (
-    patient_id integer not null,
-    last_name varchar not null ,
-    middle_name varchar not null ,
-    first_name varchar not null ,
-    birth_date integer
-);
